edd97542b8ce7c4373cba95d6f7d2fb4587f0d2e
Idan Shoshana <idan17994@gmail.com>
2020-08-05 12:23:29 +0300

    Update gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `I'm Idan Shoshana`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Idan Shoshana`,
+      summary: `I'm a Software engineer with a focus on the web world!`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `My personal site`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: '',
     },
   },
   plugins: [
