093f112c041122834a06b70c1357a801b4920cf3
Idan Shoshana <idan17994@gmail.com>
2020-08-05 12:45:15 +0300

    Update height and width of the profile picture

--- src/components/bio.js
+++ src/components/bio.js
@@ -16,7 +16,7 @@ const Bio = () => {
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 100, height: 100) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -57,11 +57,7 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        {author.summary}
       </p>
     </div>
   )
