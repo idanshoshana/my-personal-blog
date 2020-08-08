33b7285cbf9d1346c071bc2e57150ee30df7235b
Idan Shoshana <idan17994@gmail.com>
2020-08-08 18:51:56 +0300

    Add a new component that called PostsPreview

---
+++ src/components/posts-preview/posts-preview.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import styles from "./posts-preview.module.css"
+import PostPreview from "../post-preview/post-preview"
+// import { rhythm } from "../utils/typography"
+
+const PostsPreview = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              description
+              featuredImage {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <div className={styles.container}>
+      {posts.map(({ node }) => (
+        <PostPreview key={node.fields.slug} node={node} />
+      ))}
+    </div>
+  )
+}
+
+export default PostsPreview
