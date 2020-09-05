4e2d891952141d3b7cadc6e5db0cd1716b46eca1
Idan Shoshana <idan17994@gmail.com>
2020-09-05 16:02:23 +0300

    Wrap the root element with ThemeProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
+import React from "react";
+import { ThemeProvider } from "./src/context/theme-context";
 import "./src/styles/global.css";
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider>{element}</ThemeProvider>
+);
